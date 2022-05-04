87926d35e340b54ab50bf08b027abff59e9b39f1
Einspieler Sascha (KAI SDM) <sascha.einspieler@k-ai.at>
2022-05-04 16:28:30 +0200

    #1 provide clean workspace and automatic build

--- run-arara.sh
+++ run-arara.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env sh
 
+build_path="./build"
+
 while getopts c flag
 do
     case $flag in
-        c) git clean -ffdx;;	# clean everything
+        c)  git clean -ffdx     # clean everything
+            mkdir -p $build_path/src   # prepare folder structure
+            ;;
     esac
 done
 
+# prepare build folder structure
+
 # repeat 'arara' until no more 'rerunfilecheck'
 while : ; do
-	arara Thesis -v || { grep -irnE "^!" Thesis.log; exit 1; };	# abort in case arara has failed
-	grep "Warning" Thesis.log | grep -i rerun || break
+	arara Thesis -v || { grep -irnE "^!" $build_path/Thesis.log; exit 1; };	# abort in case arara has failed
+	grep "Warning" $build_path/Thesis.log | grep -i rerun || break
 done
